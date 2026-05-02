48d3ebfc9b067df52f0e6feea1a91b9fd669f432
Sphesihle Msane <msanesphesihle968@gmail.com>
2026-05-02 21:26:51 +0200

    Fix Supabase project URL

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // 🔐 REPLACE THESE WITH YOUR REAL VALUES
-const supabaseUrl = "https://YOUR_PROJECT_ID.supabase.co"
+const supabaseUrl = "https://xowegexcklulqyifmmnp.supabase.co"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhvd2VnZXhja2x1bHF5aWZtbW5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MzczMDYsImV4cCI6MjA5MzMxMzMwNn0._JoopnpYYBeXDTyYBCAjUVs166PCnI6quNMX6itG-6A"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
